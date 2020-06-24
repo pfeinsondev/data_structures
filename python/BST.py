b04cec14c81321c70885bdda4391cba7c128833d
Patrick Feinson <PFeinsonDev@gmail.com>
2020-06-24 15:39:33 -0700

    Added .find(val) to BST

--- python/BST.py
+++ python/BST.py
@@ -5,10 +5,23 @@ class Node:
 	def __init__(self, val):				# Constructor - Accepts value
 		self.val = val						# Set node value
 		self.left_child = False					# Set left_child to False
-		self.right_child = False				# Set right_child to False
+		self.right_child = False				# Set right_child to False	
 	
+	def has_left_child(self):				# has_left_child
+		if (self.left_child != False):
+			return True
+		else:
+			return False
+
+	def has_right_child(self):				# has_right_child 
+		if (self.right_child != False):
+			return True
+		else:
+			return False
+
+
 	def __str__(self):					# to string
-		return str(self.val)					# return the value of this Node
+		return str(self.val)				# return the value of this Node
 #-------#
 #  BST  #
 #-------#	
@@ -30,15 +43,35 @@ class BST:
 			else:						# If it doesn't
 				focus.right_child = Node(val)			# Add it
 
-	def print_ordered(self, focus=False):
+	def print_ascending(self, focus=False):
 		if not (focus):					# Base Case (First Pass)
 			focus = self.root				# Set focus to root
 		if (focus.left_child):				# If it has left children
-			self.print_ordered(focus.left_child) 		# Recurse left
-		print(str(focus)) 				# Print this nodes contents			
+			self.print_ascending(focus.left_child) 		# Recurse left		
+		print(focus.val)
 		if (focus.right_child):				# If it has right children
-			self.print_ordered(focus.right_child)		# Recurse right
+			self.print_ascending(focus.right_child)		# Recurse right
+	
+	def print_descending(self, focus=False):
+		if not (focus):
+			focus = self.root
+		if (focus.has_right_child()):
+			self.print_descending(focus.right_child)
+		print(focus.val)
+		if (focus.has_left_child()):
+			self.print_descending(focus.left_child)
 
+	def find(self, val, focus=False):					# Finds value in BST / Search
+		if not focus:							
+			focus = self.root						
+		if (focus.val == val):					
+			return True
+		elif (focus.has_left_child() and (focus.val > val)):
+			return self.find(val, focus.left_child)				
+		elif (focus.has_right_child() and (focus.val < val)):	
+			return self.find(val, focus.right_child)				
+		return  False							
+			
 
 #----------#
 #   Test   #
@@ -50,4 +83,4 @@ test.add(11)
 test.add(13)
 test.add(9)
 test.add(12)
-test.print_ordered()
+test.print_ascending()
